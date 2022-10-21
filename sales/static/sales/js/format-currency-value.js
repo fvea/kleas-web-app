4676475da4834828f41aca09e3c2110820b9f350
fjvin <qfveatabay@tip.edu.ph>
2022-10-21 21:32:05 +0800

    fix error on expenses tab

--- sales/static/sales/js/format-currency-value.js
+++ sales/static/sales/js/format-currency-value.js
@@ -1,5 +1,5 @@
 function check() {
-    let val = document.getElementById("price-num-input").value
+    let val = document.getElementById("price-num-input").value;
     val = val.replace(',', '');
     val = val.replace(/[^0-9,.]*/, '');
     val = parseFloat(val);
