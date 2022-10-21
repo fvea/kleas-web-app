5d6ca7c586078bba3071c587851caf053c38985d
fjvin <qfveatabay@tip.edu.ph>
2022-10-21 17:40:01 +0800

    add expenses forms

---
+++ expenses/static/expenses/js/chained_dropdown.js
@@ -0,0 +1,16 @@
+$("#category-dropdown").change(function () {
+    var url = $("#restockForm").attr("data-items-url");  // get the url of the `load_items` view
+    var categoryId = $(this).val();                   // get the selected category ID from the HTML input
+
+    $.ajax({                                          // initialize an AJAX request
+      url: url,                                       // set the url of the request (= localhost:8000/hr/ajax/load-items/)
+      data: {
+        'category': categoryId                        // add the category id to the GET parameters
+      },
+      success: function (data) {                      // `data` is the return of the `load_items' view function
+        $("#item-dropdown").html(data);
+        $("#item-dropdown").selectpicker('refresh');             // replace the contents of the items input with the data that came from the server
+      }
+    });
+
+});
--- expenses/views.py
+++ expenses/views.py
@@ -1,7 +1,38 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages 
+from django.urls import reverse
+from .models import ExpensesRestock, ExpensesStore
+from sales.models import PaymentType, Category, Item
+
 
 # Create your views here.
+def restock(request):
+    if request.method == "POST":
+        restockObj = ExpensesRestock()
+        restockObj.price = request.POST["price"]
+        restockObj.quantity = request.POST["quantity"]
+        restockObj.payment = PaymentType.objects.get(id=request.POST["payment"])
+        restockObj.category = Category.objects.get(id=request.POST["category"])
+        restockObj.item = Item.objects.get(id=request.POST["item"])
+        restockObj.save()
 
-def test(request):
-    return render(request, "expenses/test.html")
+        messages.success(request, "Restock-Expense Saved!")
 
+        return redirect(reverse("expenses:restock"))
+    else:
+        return render(request, "expenses/restock.html")
+
+
+def store(request):
+    if request.method == "POST":
+        expense = ExpensesStore()
+        expense.amount = request.POST["amount"]
+        expense.date = request.POST["date"]
+        expense.category = request.POST["category"]
+        expense.save()
+
+        messages.success(request, "Store-Expense Saved!")
+
+        return redirect(reverse("expenses:store"))
+    else:
+        return render(request, "expenses/store.html")
